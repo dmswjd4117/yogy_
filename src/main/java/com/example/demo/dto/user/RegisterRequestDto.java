8fb2347b2c1f7d1bfc0934711c40c2d4a67f763d
dmswjd4117 <dmswjd4117@naver.com>
2022-02-08 15:28:32 +0900

    refactor: RegisterRequestDto.java

--- src/main/java/com/example/demo/dto/user/RegisterRequestDto.java
+++ src/main/java/com/example/demo/dto/user/RegisterRequestDto.java
@@ -1,2 +1,11 @@
-package com.example.demo.dto.user;public class RegisterRequestDto {
+package com.example.demo.dto.user;
+
+public class RegisterRequestDto {
+    private String password;
+
+    private String email;
+
+    private String phone;
+
+    private String name;
 }
